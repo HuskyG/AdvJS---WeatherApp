054ecba0d42a58df3eace0322b9d5dbcddcd9314
Ziyue Xia <200470612@student.georgianc.on.ca>
2021-12-21 18:31:35 -0500

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,154 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <v-app>
+    <v-app-bar app flat color="rgba(0, 0, 0, 0)">
+      <v-app-bar-nav-icon
+        color="white"
+        @click="drawer = !drawer"
+      ></v-app-bar-nav-icon>
+
+      <v-toolbar-title class="text-h6 white--text pl-0">
+        Messages
+      </v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn color="white" icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-navigation-drawer v-model="drawer" absolute temporary>
+      <v-list nav dense>
+        <v-list-item-group
+          v-model="group"
+          active-class="deep-purple--text text--accent-4"
+        >
+          <v-list-item>
+            <v-list-item-title>Foo</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-title>Bar</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-title>Fizz</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-title>Buzz</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-main style="margin-top: -57px">
+      <v-carousel v-model="model" height="100vh" :show-arrows="false">
+        <v-carousel-item v-for="city in cities" :key="city">
+          <v-sheet :color="getRandomColor()" height="100%" tile>
+            <v-row class="fill-height" align="center" justify="center">
+              <div class="d-flex flex-colum">
+                <v-card-text>
+                  <h1 class="text-h3" align="center">{{ currentCity }}</h1>
+                  <p class="text-h6" align="center">{{ currentWeather }}</p>
+                  <div align="center">
+                    <img :src="icon" alt="" />
+                  </div>
+                  <p class="text-h1" align="center">{{ currentTemp }}</p>
+                </v-card-text>
+              </div>
+            </v-row>
+          </v-sheet>
+        </v-carousel-item>
+      </v-carousel>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import gql from "graphql-tag";
 
 export default {
   name: "App",
-  components: {
-    HelloWorld,
+
+  components: {},
+
+  mounted() {
+    this.fetchCities();
+  },
+
+  computed: {
+    currentCity() {
+      return this.weather?.name;
+    },
+
+    currentWeather() {
+      return this.weather?.weather?.summary?.title;
+    },
+
+    currentTemp() {
+      const temp = this.weather?.weather?.temperature?.actual;
+      return temp ? Math.round(temp) : "-";
+    },
+
+    icon() {
+      const iconCode = this.weather?.weather?.summary?.icon;
+
+      return iconCode
+        ? `https://openweathermap.org/img/wn/${iconCode}@4x.png`
+        : "";
+    },
+  },
+
+  data: () => ({
+    model: 0,
+    cities: ["Barrie"],
+    drawer: false,
+    group: null,
+    weather: {},
+  }),
+
+  methods: {
+    fetchCities() {
+      this.cities = ["Barrie", "Toronto", "Tokyo", "Bloomington", "GuangZhou"];
+    },
+
+    getRandomInt(max = 128) {
+      return Math.floor(Math.random() * Math.floor(max));
+    },
+
+    getRandomColor() {
+      return `rgb(${this.getRandomInt()}, ${this.getRandomInt()},${this.getRandomInt()})`;
+    },
+  },
+
+  apollo: {
+    weather: {
+      query: gql`
+        query getWeather($city: String!) {
+          weather: getCityByName(name: $city) {
+            name
+            weather {
+              summary {
+                title
+                icon
+              }
+            }
+            weather {
+              temperature {
+                actual
+              }
+            }
+          }
+        }
+      `,
+
+      variables() {
+        return {
+          city: this.cities[this.model],
+        };
+      },
+    },
   },
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
